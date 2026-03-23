b69d73579fa871aff0870430d570410d75ff8baf
David-ABS <david.aboissiere@gmail.com>
2026-03-23 18:20:37 +0100

    SQLlite
    Création fichier sqlite  base de données
    + script  set_up  DB

---
+++ setup_db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+# 1. Créer le fichier de la base de données (il va se créer tout seul)
+connexion = sqlite3.connect('label_vivre.sqlite')
+curseur = connexion.cursor()
+
+# 2. Lire TON fichier SQL
+with open('SCRIPT_CREATION_BDD_V1.sql', 'r', encoding='utf-8') as fichier_sql:
+    script_sql = fichier_sql.read()
+
+# 3. Exécuter le code SQL pour créer toutes les tables
+curseur.executescript(script_sql)
+
+# 4. Valider et fermer
+connexion.commit()
+connexion.close()
+
+print("Succès ! La base de données label_vivre.sqlite a été créée avec toutes tes tables.")
